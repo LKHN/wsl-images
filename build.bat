3b13d7334683e1d5bd3b45ba9df4ced2f5e30e17
Elkhan Mammadli <elkhan.mammadli@protonmail.com>
2024-08-23 17:05:00 +0400

    wip: apply naming conventions
    Signed-off-by: Elkhan Mammadli <elkhan.mammadli@protonmail.com>

--- build.bat
+++ build.bat
@@ -79,7 +79,7 @@ goto :ARGS_LOOP
 
 if (%ERRORLEVEL%) == (0) (
     echo.
-    echo Created appx in %~dp0x64\%_MSBUILD_CONFIG%\AlmaLinuxOS-8\
+    echo Created appx in %~dp0x64\%_MSBUILD_CONFIG%\AlmaLinux-8\
     echo.
 )
 
